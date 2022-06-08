1dd186d7e33321e5e77beddc9761b5c3c781cd4b
coderinblack08 <kevinlu.email@gmail.com>
2022-06-08 13:52:02 -0700

    refactor: clear branch

--- apps/web/lib/swr-fetcher.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetcher = async (url: string) => {
-  const res = await axios.get(url, { withCredentials: true });
-  if (!res.status.toString().startsWith("2")) {
-    throw new Error(res.statusText);
-  }
-  return res.data;
-};
--- apps/web/next.config.js
+++
@@ -1,8 +0,0 @@
-const withTM = require("next-transpile-modules")(["ui"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-  experimental: {
-    esmExternals: false,
-  },
-});
